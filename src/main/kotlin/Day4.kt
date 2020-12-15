1c4ce4cacb9d9a5ec4e194d94fad258306b638d5
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-16 00:54:25 +0100

    Day 4 Advent of Code

---
+++ src/main/kotlin/Day4.kt
@@ -0,0 +1,78 @@
+import java.io.File
+import java.util.concurrent.Callable
+import java.util.function.Predicate
+
+fun main() {
+    val lines = File("aoc2020/day4.txt").readLines()
+
+    println("day4part1=${micros(Callable { day4part1(lines) })}")
+    println("day4part2=${micros(Callable { day4part2(lines) })}")
+}
+
+val MANDATORY = hashMapOf<String, Predicate<String>>(
+    "byr" to Predicate { it.length == 4 && it.toInt() in 1920..2002 },
+    "iyr" to Predicate { it.length == 4 && it.toInt() in 2010..2020 },
+    "eyr" to Predicate { it.length == 4 && it.toInt() in 2020..2030 },
+    "hgt" to Predicate {
+        it.endsWith("cm") && it.substring(0, it.indexOf("cm")).toInt() in 150..193
+                || (it.endsWith("in") && it.substring(0, it.indexOf("in")).toInt() in 59..76)
+    },
+    "hcl" to Predicate { it.matches("^#[0-9a-f]{6}$".toRegex()) },
+    "ecl" to Predicate {
+        it == "amb" || it == "blu" || it == "brn" ||
+                it == "gry" || it == "grn" || it == "hzl" || it == "oth"
+    },
+    "pid" to Predicate { it.matches("^[0-9]{9}$".toRegex()) }
+)
+
+fun day4part1(lines: List<String>): Long {
+    return parsePassports(lines)
+        .stream()
+        .filter { isValidPassport(it) }
+        .count()
+}
+
+fun day4part2(lines: List<String>): Long {
+    return parsePassports(lines)
+        .stream()
+        .filter { isValidPassport2(it) }
+        .count()
+}
+
+private fun isValidPassport(passport: Passport): Boolean {
+    for (key in MANDATORY.keys) when {
+        !passport.fields.containsKey(key) -> return false
+    }
+    return true
+}
+
+private fun isValidPassport2(passport: Passport): Boolean {
+    for (man in MANDATORY.entries) when {
+        !passport.fields.containsKey(man.key) -> return false
+        passport.fields[man.key].isNullOrBlank() -> return false
+        !man.value.test(passport.fields[man.key]!!) -> return false
+    }
+    return true
+}
+
+private fun parsePassports(lines: List<String>): ArrayList<Passport> {
+    val passports = arrayListOf<Passport>()
+    var cur = 0
+    while (cur < lines.size) {
+        val pass = Passport()
+        while (cur < lines.size && lines[cur].isNotBlank()) {
+            lines[cur].split(" ").forEach {
+                val vals = it.split(":")
+                pass.fields[vals[0]] = vals[1]
+            }
+            cur++
+        }
+        passports += pass
+        cur++
+    }
+    return passports
+}
+
+class Passport {
+    val fields = hashMapOf<String, String>()
+}
