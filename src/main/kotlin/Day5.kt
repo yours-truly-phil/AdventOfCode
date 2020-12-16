4d85be04c0752a66043c1e2e5e42180c1bce8c8c
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-16 20:52:47 +0100

    Day 5 Advent of Code

---
+++ src/main/kotlin/Day5.kt
@@ -0,0 +1,38 @@
+import java.io.File
+import java.util.concurrent.Callable
+
+fun main() {
+    val lines = File("aoc2020/day5.txt").readLines()
+
+    println("day4part1=${micros(Callable { part1(lines) })}")
+    println("day4part2=${micros(Callable { part2(lines, 12, 858) })}")
+}
+
+fun part1(lines: List<String>):Int {
+    return lines.map { seatIdOf(it) }.max()!!
+}
+
+fun part2(lines: List<String>, low: Int, high: Int):Int {
+    val sorted = lines.map { seatIdOf(it) }.sorted()
+    for (it in low..high) when {
+        sorted[it-low] != it -> return it
+    }
+    return -1
+}
+
+fun seatIdOf(line: String):Int {
+    val row = biSearch(line.substring(0, 7), 'F', 'B')
+    val col = biSearch(line.substring(7), 'L', 'R')
+    return row * 8 + col
+}
+
+fun biSearch(trace: String, low: Char, high: Char): Int {
+    val steps = trace.length
+    var lower = 0
+    var upper = (1 shl steps) - 1
+    for(i in 0 until steps) when {
+        trace[i] == low -> upper = lower + (upper - lower) / 2
+        trace[i] == high -> lower = upper - (upper - lower) / 2
+    }
+    return lower
+}
---
+++ src/test/kotlin/Day5KtTest.kt
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day5KtTest {
+
+    @Test
+    fun search() {
+        assertEquals(biSearch("FBFBBFF", 'F', 'B'), 44)
+        assertEquals(biSearch("RLR", 'L', 'R'), 5)
+    }
+
+    @Test
+    fun seatIdOf() {
+        assertEquals(seatIdOf("BFFFBBFRRR"), 567)
+        assertEquals(seatIdOf("FFFBBBFRRR"), 119)
+        assertEquals(seatIdOf("BBFFBBFRLL"), 820)
+        assertEquals(seatIdOf("FFBFBFFLRL"), 162)
+    }
+
+    @Test
+    fun calculations() {
+        assertEquals((1 shl 7) - 1, 127)
+        assertEquals(127 / 2, 63)
+        assertEquals(63 - 63 / 2, 32)
+        assertEquals(32 + (63 - 32) / 2, 47)
+    }
+}
