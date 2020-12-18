73a8b84d2e3ac670c6a567b74fe0dc1f50e9c7e2
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-18 21:48:17 +0100

    Day 3 Part 2 Aoc 2015

--- src/main/kotlin/aoc2015/Day3.kt
+++ src/main/kotlin/aoc2015/Day3.kt
@@ -5,6 +5,7 @@ import java.io.File
 fun main() {
     val day3 = Day3("files/2015/day3.txt")
     println(day3.part1())
+    println(day3.part2())
 }
 
 class Day3(path: String) {
@@ -21,10 +22,31 @@ class Day3(path: String) {
                 '^' -> y++
                 'v' -> y--
             }
-            houses.add(House(x,y))
+            houses.add(House(x, y))
+        }
+        return houses.size
+    }
+
+    fun part2(): Int {
+        val santa = Santa(0, 0)
+        val robot = Santa(0, 0)
+        val houses = HashSet<House>()
+        houses.add(House(santa.x, santa.y))
+        text.forEachIndexed { i, c ->
+            var cur = santa
+            if (i % 2 == 1) cur = robot
+            when (c) {
+                '>' -> cur.x++
+                '<' -> cur.x--
+                '^' -> cur.y++
+                'v' -> cur.y--
+            }
+            houses.add(House(cur.x, cur.y))
         }
         return houses.size
     }
 
     data class House(var x: Int, var y: Int)
+
+    class Santa(var x: Int, var y: Int)
 }
