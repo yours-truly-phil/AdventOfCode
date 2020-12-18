ec94336aa1c8af02d110c2069b2637c32122fefa
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-18 21:34:13 +0100

    Day 2 Part 2 Aoc 2015

--- src/main/kotlin/aoc2015/Day2.kt
+++ src/main/kotlin/aoc2015/Day2.kt
@@ -5,6 +5,7 @@ import java.io.File
 fun main() {
     val day2 = Day2("files/2015/day2.txt")
     println(day2.part1())
+    println(day2.part2())
 }
 
 class Day2(path: String) {
@@ -12,6 +13,10 @@ class Day2(path: String) {
 
     fun part1(): Int {
         return lines.map { Box(it).calcWrappingAmount() }.sum()
+    }
+
+    fun part2(): Int {
+        return lines.map { Box(it).ribbon() }.sum()
     }
 
     class Box(line: String) {
@@ -23,6 +28,11 @@ class Day2(path: String) {
             return 3 * dims[0] * dims[1] +
                     2 * dims[1] * dims[2] +
                     2 * dims[0] * dims[2]
+        }
+
+        fun ribbon(): Int {
+            return dims[0] * dims[1] * dims[2] +
+                    2 * dims[0] + 2 * dims[1]
         }
     }
 }
