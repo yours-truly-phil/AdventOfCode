648920a816e15a8dbcc36b8432438231589bde31
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-18 21:18:59 +0100

    Day 1 Part 1 AoC 2015

--- src/main/kotlin/aoc2015/Day1.kt
+++ src/main/kotlin/aoc2015/Day1.kt
@@ -1,13 +1,23 @@
 package aoc2015
 
+import java.io.File
+
 fun main() {
-    runDay1()
+    val day1 = Day1("files/2015/day1.txt")
+    println(day1.part1())
 }
 
-fun runDay1() {
+class Day1(path: String) {
+    val line: String = File(path).readText()
 
-}
-
-class Day1 {
-
+    fun part1(): Int {
+        var floor = 0
+        line.forEach {
+            when (it) {
+                '(' -> floor++
+                ')' -> floor--
+            }
+        }
+        return floor
+    }
 }
