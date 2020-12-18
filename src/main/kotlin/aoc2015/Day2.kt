fc35f613099559b31d00e7006bf2446b5d510658
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-18 21:30:50 +0100

    Day 2 Part 1 AoC 2015

---
+++ src/main/kotlin/aoc2015/Day2.kt
@@ -0,0 +1,28 @@
+package aoc2015
+
+import java.io.File
+
+fun main() {
+    val day2 = Day2("files/2015/day2.txt")
+    println(day2.part1())
+}
+
+class Day2(path: String) {
+    val lines = File(path).readLines()
+
+    fun part1(): Int {
+        return lines.map { Box(it).calcWrappingAmount() }.sum()
+    }
+
+    class Box(line: String) {
+        private val dims = line.split("x")
+                .map { it.toInt() }
+                .sorted()
+
+        fun calcWrappingAmount(): Int {
+            return 3 * dims[0] * dims[1] +
+                    2 * dims[1] * dims[2] +
+                    2 * dims[0] * dims[2]
+        }
+    }
+}
