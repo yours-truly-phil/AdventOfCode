60a34edae07724b63f340b16462ce8b775747244
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-18 21:22:03 +0100

    Day 1 Part 2 AoC 2015

--- src/main/kotlin/aoc2015/Day1.kt
+++ src/main/kotlin/aoc2015/Day1.kt
@@ -5,6 +5,7 @@ import java.io.File
 fun main() {
     val day1 = Day1("files/2015/day1.txt")
     println(day1.part1())
+    println(day1.part2())
 }
 
 class Day1(path: String) {
@@ -19,5 +20,19 @@ class Day1(path: String) {
             }
         }
         return floor
+    }
+
+    fun part2(): Int {
+        var floor = 0
+        for (i in line.indices) {
+            when (line[i]) {
+                '(' -> floor++
+                ')' -> floor--
+            }
+            if (floor < 0) {
+                return i + 1
+            }
+        }
+        return -1
     }
 }
