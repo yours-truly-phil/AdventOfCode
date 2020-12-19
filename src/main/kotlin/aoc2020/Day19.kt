4dc10fb468d9b6a5dfc8216bfb2ade3b4816f6de
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-19 22:17:20 +0100

    Day 19 Part 2 Advent of Code

--- src/main/kotlin/aoc2020/Day19.kt
+++ src/main/kotlin/aoc2020/Day19.kt
@@ -1,2 +1,135 @@
 package aoc2020
 
+import java.io.File
+
+fun main() {
+    val day19 = Day19(File("files/2020/day19.txt").readText())
+    println(day19.part1())
+    val day19part2 = Day19(File("files/2020/day19Part2.txt").readText())
+    println(day19part2.part2())
+}
+
+class Day19(content: String) {
+
+    val rulesMap: HashMap<Int, Rule>
+    val codes: List<String>
+
+    init {
+        val fileParts = content.split("\n\n")
+        val rulesPart = fileParts[0]
+        codes = fileParts[1].lines()
+
+        rulesMap = HashMap()
+        rulesPart.lines()
+                .map { Rule(it) }
+                .map { it.no to it }.toMap(rulesMap)
+
+        for (rule in rulesMap.values) {
+            for (listOfRuleNums in rule.links) {
+                val linkedRuleList = ArrayList<Rule>()
+                for (ruleNum in listOfRuleNums) {
+                    val linkedRule = rulesMap[ruleNum]!!
+                    linkedRuleList.add(linkedRule)
+                }
+                rule.rules.add(linkedRuleList)
+            }
+        }
+    }
+
+    fun part1(): Int {
+        val rule0 = rulesMap[0]!!
+        var count = 0
+        val regex = "^${rule0.toRegex(rulesMap)}$".toRegex()
+//        println(regex)
+        for (code in codes) {
+            if (regex.matches(code)) count++
+        }
+        return count
+    }
+
+    fun part2(): Int {
+        val rule0 = rulesMap[0]!!
+        var count = 0
+        val regex = "^${rule0.toRegexPart2(rulesMap)}$".toRegex()
+//        println(regex)
+        for (code in codes) {
+            if (regex.matches(code)) count++
+        }
+        return count
+    }
+
+    class Rule(s: String) {
+        val no: Int
+        val links = ArrayList<List<Int>>()
+        val rules = ArrayList<List<Rule>>()
+        var value: String = ""
+
+        init {
+            var parts = s.split(":")
+            no = parts[0].toInt()
+            when {
+                parts[1].contains("a") -> value = "a"
+                parts[1].contains("b") -> value = "b"
+                else -> {
+                    parts = parts[1].split("|")
+                    parts.mapTo(links) { nums ->
+                        nums.split(" ").filter { it.isNotEmpty() }
+                                .map { it.toInt() }.toList()
+                    }
+                }
+            }
+        }
+
+        fun toRegexPart2(rulesMap: Map<Int, Rule>): String {
+            if (value.isNotEmpty()) {
+                return value
+            } else {
+                var res = "("
+                when (no) {
+                    8 -> {
+                        res += "${rules[0][0].toRegexPart2(rulesMap)}+"
+                    }
+                    11 -> {
+                        val left = rules[0][0].toRegexPart2(rulesMap)
+                        val right = rules[0][1].toRegexPart2(rulesMap)
+                        res += "($left$right)"
+                        for (i in 2..10) {
+                            res += "|("
+                            for (j in 1..i) {
+                                res += left
+                            }
+                            for (j in 1..i) {
+                                res += right
+                            }
+                            res += ")"
+                        }
+                    }
+                    else -> {
+                        res += rules.joinToString(separator = "|") {
+                            "(${it.map { it.toRegexPart2(rulesMap) }.joinToString(separator = "") { it }})"
+                        }
+                    }
+                }
+                res += ")"
+                return res
+            }
+        }
+
+        fun toRegex(rulesMap: Map<Int, Rule>): String {
+            return if (value.isNotEmpty()) {
+                value
+            } else {
+                var res = "("
+                res += rules.joinToString(separator = "|") {
+                    "(${it.map { it.toRegex(rulesMap) }.joinToString(separator = "") { it }})"
+                }
+                res += ")"
+                res
+            }
+        }
+
+        override fun toString(): String {
+            return "Rule $no $value links=$links)"
+        }
+    }
+}
--- src/test/kotlin/aoc2020/Day19Test.kt
+++ src/test/kotlin/aoc2020/Day19Test.kt
@@ -1,12 +1,131 @@
 package aoc2020
 
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
+import kotlin.test.assertEquals
 
 internal class Day19Test {
 
     @Test
-    fun findCharsAt() {
+    fun part1() {
+        val content = """
+            0: 4 1 5
+            1: 2 3 | 3 2
+            2: 4 4 | 5 5
+            3: 4 5 | 5 4
+            4: "a"
+            5: "b"
+
+            ababbb
+            bababa
+            abbbab
+            aaabbb
+            aaaabbb""".trimIndent()
+        val day19 = Day19(content)
+        assertEquals(2, day19.part1())
+    }
+
+    @Test
+    fun part2() {
+        val content = """
+            42: 9 14 | 10 1
+            9: 14 27 | 1 26
+            10: 23 14 | 28 1
+            1: "a"
+            11: 42 31
+            5: 1 14 | 15 1
+            19: 14 1 | 14 14
+            12: 24 14 | 19 1
+            16: 15 1 | 14 14
+            31: 14 17 | 1 13
+            6: 14 14 | 1 14
+            2: 1 24 | 14 4
+            0: 8 11
+            13: 14 3 | 1 12
+            15: 1 | 14
+            17: 14 2 | 1 7
+            23: 25 1 | 22 14
+            28: 16 1
+            4: 1 1
+            20: 14 14 | 1 15
+            3: 5 14 | 16 1
+            27: 1 6 | 14 18
+            14: "b"
+            21: 14 1 | 1 14
+            25: 1 1 | 1 14
+            22: 14 14
+            8: 42
+            26: 14 22 | 1 20
+            18: 15 15
+            7: 14 5 | 1 21
+            24: 14 1
+
+            abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+            bbabbbbaabaabba
+            babbbbaabbbbbabbbbbbaabaaabaaa
+            aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+            bbbbbbbaaaabbbbaaabbabaaa
+            bbbababbbbaaaaaaaabbababaaababaabab
+            ababaaaaaabaaab
+            ababaaaaabbbaba
+            baabbaaaabbaaaababbaababb
+            abbbbabbbbaaaababbbbbbaaaababb
+            aaaaabbaabaaaaababaa
+            aaaabbaaaabbaaa
+            aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+            babaaabbbaaabaababbaabababaaab
+            aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba""".trimIndent()
+        val day19 = Day19(content)
+        println(day19.part2())
+        val contentUpdated = """
+            42: 9 14 | 10 1
+            9: 14 27 | 1 26
+            10: 23 14 | 28 1
+            1: "a"
+            11: 42 31 | 42 11 31
+            5: 1 14 | 15 1
+            19: 14 1 | 14 14
+            12: 24 14 | 19 1
+            16: 15 1 | 14 14
+            31: 14 17 | 1 13
+            6: 14 14 | 1 14
+            2: 1 24 | 14 4
+            0: 8 11
+            13: 14 3 | 1 12
+            15: 1 | 14
+            17: 14 2 | 1 7
+            23: 25 1 | 22 14
+            28: 16 1
+            4: 1 1
+            20: 14 14 | 1 15
+            3: 5 14 | 16 1
+            27: 1 6 | 14 18
+            14: "b"
+            21: 14 1 | 1 14
+            25: 1 1 | 1 14
+            22: 14 14
+            8: 42 | 42 8
+            26: 14 22 | 1 20
+            18: 15 15
+            7: 14 5 | 1 21
+            24: 14 1
+
+            abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+            bbabbbbaabaabba
+            babbbbaabbbbbabbbbbbaabaaabaaa
+            aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+            bbbbbbbaaaabbbbaaabbabaaa
+            bbbababbbbaaaaaaaabbababaaababaabab
+            ababaaaaaabaaab
+            ababaaaaabbbaba
+            baabbaaaabbaaaababbaababb
+            abbbbabbbbaaaababbbbbbaaaababb
+            aaaaabbaabaaaaababaa
+            aaaabbaaaabbaaa
+            aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+            babaaabbbaaabaababbaabababaaab
+            aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba""".trimIndent()
+        val day19Part2 = Day19(contentUpdated)
+        assertEquals(12, day19Part2.part2())
+        println(day19Part2.part2())
     }
 }
