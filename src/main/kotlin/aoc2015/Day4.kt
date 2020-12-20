a70bc6790068ede077241924b4b8e85bb92b5846
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-20 01:12:18 +0100

    Day 4 Part 2 AoC 2015

--- src/main/kotlin/aoc2015/Day4.kt
+++ src/main/kotlin/aoc2015/Day4.kt
@@ -6,12 +6,21 @@ import java.security.MessageDigest
 fun main() {
     val day4 = Day4(File("files/2015/day4.txt").readText())
     println(day4.part1())
+    println(day4.part2())
 }
 
 class Day4(val input: String) {
     fun part1(): Int {
         var num = 1
         while (!md5(input.plus(num)).toHex().startsWith("00000")) {
+            num++
+        }
+        return num
+    }
+
+    fun part2(): Int {
+        var num = 1
+        while (!md5(input.plus(num)).toHex().startsWith("000000")) {
             num++
         }
         return num
