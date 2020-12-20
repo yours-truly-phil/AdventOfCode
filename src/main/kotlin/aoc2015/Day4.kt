69ed0affe35004a69350a61360d15fcd9b51f9fe
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-20 01:08:48 +0100

    Day 4 Part 1 AoC 2015

---
+++ src/main/kotlin/aoc2015/Day4.kt
@@ -0,0 +1,24 @@
+package aoc2015
+
+import java.io.File
+import java.security.MessageDigest
+
+fun main() {
+    val day4 = Day4(File("files/2015/day4.txt").readText())
+    println(day4.part1())
+}
+
+class Day4(val input: String) {
+    fun part1(): Int {
+        var num = 1
+        while (!md5(input.plus(num)).toHex().startsWith("00000")) {
+            num++
+        }
+        return num
+    }
+
+    private fun md5(str: String): ByteArray = MessageDigest.getInstance("MD5")
+            .digest(str.toByteArray(Charsets.UTF_8))
+
+    private fun ByteArray.toHex() = joinToString("") { "%02x".format(it) }
+}
