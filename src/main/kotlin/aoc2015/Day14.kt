8bc847bfc7d95579bb044557c3adb997db956870
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-26 20:43:20 +0100

    Day 14 Part 2 AoC 2015

--- src/main/kotlin/aoc2015/Day14.kt
+++ src/main/kotlin/aoc2015/Day14.kt
@@ -4,17 +4,46 @@ import java.io.File
 
 fun main() {
     Day14().also { println(it.part1(File("files/2015/day14.txt").readText())) }
+        .also { println(it.part2(File("files/2015/day14.txt").readText())) }
 }
 
 class Day14 {
     fun part1(input: String): Long {
-        val reindeer = input.lines().map { Reindeer(it) }
+        val reindeer = parse(input)
         repeat(2503) {
             reindeer.forEach { it.step() }
         }
+        return winnerDistance(reindeer)
+    }
+
+    private fun winnerDistance(reindeer: List<Reindeer>): Long {
         var max = Long.MIN_VALUE
         reindeer.forEach { max = maxOf(max, it.dist) }
         return max
+    }
+
+    fun part2(input: String): Long {
+        val reindeer = parse(input)
+        repeat(2503) {
+            var leadDist = 0L
+            reindeer.forEach {
+                it.step()
+                leadDist = maxOf(it.dist, leadDist)
+            }
+
+            reindeer.filter { it.dist == leadDist }.forEach { it.points++ }
+        }
+        return winnerPoints(reindeer)
+    }
+
+    private fun winnerPoints(reindeer: List<Reindeer>): Long {
+        var max = Long.MIN_VALUE
+        reindeer.forEach { max = maxOf(max, it.points) }
+        return max
+    }
+
+    private fun parse(input: String): List<Reindeer> {
+        return input.lines().map { Reindeer(it) }
     }
 
     class Reindeer(input: String) {
@@ -26,6 +55,7 @@ class Day14 {
         var flyTime = 0
         var restTime = 0
         var dist = 0L
+        var points = 0L
 
         init {
             input.split(" ")
@@ -50,6 +80,21 @@ class Day14 {
                     restTime = 0
                 }
             }
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Reindeer
+
+            if (name != other.name) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            return name.hashCode()
         }
     }
 
