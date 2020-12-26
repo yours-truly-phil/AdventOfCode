99babe0256bd3767cae7a12710befc8f3044c7f9
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-26 08:37:09 +0100

    Day 8 Part 1 AoC 2015

---
+++ src/main/kotlin/aoc2015/Day8.kt
@@ -0,0 +1,40 @@
+package aoc2015
+
+import java.io.File
+
+fun main() {
+    Day8()
+        .also { println(it.part1(File("files/2015/day8.txt").readText())) }
+}
+
+class Day8 {
+    fun part1(input: String): Int {
+        val noChars = input.lines().map { it.length }.sum()
+        val noUnescapedChars = input.lines().map { unescapedStringLength(it) }.sum()
+        return noChars - noUnescapedChars
+    }
+
+    private fun unescapedStringLength(s: String): Int {
+        var i = 0
+        var count = 0
+        while (i < s.length) {
+            val r = s.substring(i)
+            when {
+                s[i] == '"' -> i++
+                r.startsWith("\\x") -> {
+                    i += 4
+                    count++
+                }
+                r.startsWith("\\") -> {
+                    i += 2
+                    count++
+                }
+                else -> {
+                    i++
+                    count++
+                }
+            }
+        }
+        return count
+    }
+}
