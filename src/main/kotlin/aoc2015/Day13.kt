748734bb119b72b2f9b096a6657a056b1c8c5084
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-26 20:05:38 +0100

    Day 13 Part 1 AoC 2015

---
+++ src/main/kotlin/aoc2015/Day13.kt
@@ -0,0 +1,74 @@
+package aoc2015
+
+import java.io.File
+
+fun main() {
+    Day13().also { println(it.part1(File("files/2015/day13.txt").readText())) }
+}
+
+class Day13 {
+    fun part1(input: String): Int {
+
+        val people = input.lines()
+            .map { it.split(" ")[0] }
+            .distinct()
+            .map { it to Person(it) }.toMap()
+
+        input.lines()
+            .forEach {
+                val parts = it.split(" ")
+                val person = parts[0]
+                val nextTo = parts[10].substring(0, parts[10].indexOf("."))
+                var amount = parts[3].toInt()
+                if (parts[2] == "lose") amount *= -1
+                people[person]!!.happiness[nextTo] = amount
+
+            }
+        for (entry in people) {
+            println("${entry.key} -> ${entry.value.happiness}")
+        }
+
+        val listOfPeople = people.keys.toList()
+        val permutations = listOfPeople.size.pow(listOfPeople.size)
+        var res = Int.MIN_VALUE
+        for (i in 0 until permutations) {
+            val seating = i.toString(listOfPeople.size).padStart(listOfPeople.size, '0')
+            var possibleSeating = true
+            for (p in listOfPeople.indices) {
+                if (seating.filter { Integer.parseInt(it.toString()) == p }.count() != 1) {
+                    possibleSeating = false
+                    break
+                }
+            }
+            if (!possibleSeating) continue
+
+            var happiness = 0
+            for (seatNo in seating.indices) {
+                val idx = seating[seatNo].toString().toInt()
+                val person = listOfPeople[idx]
+
+                val neighborLeft =
+                    listOfPeople[seating[(seatNo - 1 + seating.length) % seating.length].toString().toInt()]
+                val neighborRight =
+                    listOfPeople[seating[(seatNo + 1) % seating.length].toString().toInt()]
+                happiness += people[person]!!.happiness[neighborLeft]!!
+                happiness += people[person]!!.happiness[neighborRight]!!
+            }
+            res = maxOf(res, happiness)
+        }
+        return res
+    }
+
+    data class Person(val name: String) {
+        val happiness = HashMap<String, Int>()
+    }
+
+    private fun Int.pow(other: Int): Long {
+        var res = 1L
+        repeat(other) {
+            res *= other
+        }
+        return res
+    }
+}
+
---
+++ src/test/kotlin/aoc2015/Day13Test.kt
@@ -0,0 +1,24 @@
+package aoc2015
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day13Test {
+
+    @Test
+    fun part1() {
+        val input = "Alice would gain 54 happiness units by sitting next to Bob.\n" +
+                "Alice would lose 79 happiness units by sitting next to Carol.\n" +
+                "Alice would lose 2 happiness units by sitting next to David.\n" +
+                "Bob would gain 83 happiness units by sitting next to Alice.\n" +
+                "Bob would lose 7 happiness units by sitting next to Carol.\n" +
+                "Bob would lose 63 happiness units by sitting next to David.\n" +
+                "Carol would lose 62 happiness units by sitting next to Alice.\n" +
+                "Carol would gain 60 happiness units by sitting next to Bob.\n" +
+                "Carol would gain 55 happiness units by sitting next to David.\n" +
+                "David would gain 46 happiness units by sitting next to Alice.\n" +
+                "David would lose 7 happiness units by sitting next to Bob.\n" +
+                "David would gain 41 happiness units by sitting next to Carol."
+        assertEquals(330, Day13().part1(input))
+    }
+}
