0bfbe07fc61b5b31135b59d3ae2d2867cfa03c01
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-26 15:34:06 +0100

    Day 10 Part 2 AoC 2015

--- src/main/kotlin/aoc2015/Day10.kt
+++ src/main/kotlin/aoc2015/Day10.kt
@@ -4,19 +4,25 @@ import java.io.File
 
 fun main() {
     Day10().also { println(it.part1(File("files/2015/day10.txt").readText())) }
+        .also { println(it.part2(File("files/2015/day10.txt").readText())) }
 }
 
 class Day10 {
-    fun part1(input: String): Int {
+    fun part1(input: String): Int = calcSequence(input, 40).length
+
+    fun part2(input: String): Int = calcSequence(input, 50).length
+
+    private fun calcSequence(input: String, count: Int): String {
         var cur = input
-        repeat(40) {
+        for (i in 1..count) {
             cur = next(cur)
+            println("$i=${cur.length}")
         }
-        return cur.length
+        return cur
     }
 
     fun next(s: String): String {
-        var res = ""
+        val sb = StringBuilder()
         var i = 0
         while (i < s.length) {
             val next = s[i]
@@ -28,11 +34,11 @@ class Day10 {
                     end++
                 }
             }
-            res += end - i
-            res += next
+            sb.append(end - i)
+            sb.append(next)
             i = end
         }
-        return res
+        return sb.toString()
     }
 
 }
