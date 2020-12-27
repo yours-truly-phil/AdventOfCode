580f6afb4a99d418b7c2a9503a5f99e23842c81a
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-27 14:23:59 +0100

    Add guava lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
     implementation("com.google.code.gson:gson:2.8.6")
+    implementation("com.google.guava:guava:30.1-jre")
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
--- src/test/kotlin/aoc2015/Day17Test.kt
+++ src/test/kotlin/aoc2015/Day17Test.kt
@@ -1,5 +1,6 @@
 package aoc2015
 
+import com.google.common.collect.Sets
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -8,5 +9,14 @@ internal class Day17Test {
     @Test
     fun part1() {
         Day17().perms(25, intArrayOf(20, 15, 10, 5, 5)).also { assertEquals(4, it) }
+    }
+
+    @Test
+    fun guava() {
+        val input = intArrayOf(20, 15, 10, 5, 5)
+        Sets.powerSet(input.indices.toSet())
+            .map { it.map { idx -> input[idx] } }
+            .filter { it.isNotEmpty() }
+            .forEach { println(it) }
     }
 }
