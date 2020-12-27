40842d07f048f4317aff6ea85a611bf38b9c51ca
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-27 11:06:32 +0100

    Day 16 Part 2 AoC 2015

--- src/main/kotlin/aoc2015/Day16.kt
+++ src/main/kotlin/aoc2015/Day16.kt
@@ -4,16 +4,30 @@ import java.io.File
 
 fun main() {
     Day16().also { println(it.part1(File("files/2015/day16.txt").readText())) }
+        .also { println(it.part2(File("files/2015/day16.txt").readText())) }
 }
 
 class Day16 {
+    private val present = hashMapOf(
+        "children" to 3,
+        "cats" to 7, "samoyeds" to 2, "pomeranians" to 3, "akitas" to 0, "vizslas" to 0,
+        "goldfish" to 5, "trees" to 3, "cars" to 2, "perfumes" to 1
+    )
+
     fun part1(input: String): Sue {
-        val present = hashMapOf(
-            "children" to 3,
-            "cats" to 7, "samoyeds" to 2, "pomeranians" to 3, "akitas" to 0, "vizslas" to 0,
-            "goldfish" to 5, "trees" to 3, "cars" to 2, "perfumes" to 1
-        )
         return input.lines().map { Sue(it) }.first { it.attributes.all { a -> present[a.key] == a.value } }
+    }
+
+    fun part2(input: String): Sue {
+        return input.lines().map { Sue(it) }.first { it.attributes.all { a -> isPresentSue(a) } }
+    }
+
+    private fun isPresentSue(a: Map.Entry<String, Int>) = when (a.key) {
+        "cats" -> present[a.key]!! < a.value
+        "trees" -> present[a.key]!! < a.value
+        "pomeranians" -> present[a.key]!! > a.value
+        "goldfish" -> present[a.key]!! > a.value
+        else -> present[a.key] == a.value
     }
 
     class Sue(input: String) {
