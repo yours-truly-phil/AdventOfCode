e1257bc242cfe84dbdfb9e6cfe049ebcb30fe93f
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-27 13:51:02 +0100

    Day 17 Part 2 AoC 2015

--- src/main/kotlin/aoc2015/Day17.kt
+++ src/main/kotlin/aoc2015/Day17.kt
@@ -5,10 +5,22 @@ import java.io.File
 
 fun main() {
     Day17().also { println(it.part1(File("files/2015/day17.txt").readText())) }
+        .also { println(it.part2(File("files/2015/day17.txt").readText())) }
 }
 
 class Day17 {
     fun part1(input: String): Int = perms(150, input.lines().map { it.toInt() }.toIntArray())
+
+    fun part2(input: String): Int = perms2(150, input.lines().map { it.toInt() }.toIntArray())
+
+    fun perms2(num: Int, nums: IntArray): Int {
+        val res = arrayListOf(ArrayList<Int>())
+            .also { combinations(nums, it) }
+            .also { it.sortBy { comb -> comb.size } }
+            .filter { it.sum() == num }
+        val minLength = res.minOf { it.size }
+        return res.filter { it.size == minLength }.count()
+    }
 
     fun perms(num: Int, nums: IntArray): Int = arrayListOf(ArrayList<Int>())
         .also { combinations(nums, it) }
