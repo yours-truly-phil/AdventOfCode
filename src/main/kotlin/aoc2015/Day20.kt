8d1ab32c1fe35f2b59894aa8f2c3918fb1b20f51
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-27 22:59:23 +0100

    Prepare Day 20 AoC 2015

--- src/main/kotlin/aoc2015/Day20.kt
+++ src/main/kotlin/aoc2015/Day20.kt
@@ -1,2 +1,13 @@
 package aoc2015
 
+import java.io.File
+
+fun main() {
+    Day20().also { println(it.part1(File("files/2015/day20.txt").readText())) }
+}
+
+class Day20 {
+    fun part1(input: String): Int {
+        return -1
+    }
+}
