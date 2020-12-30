f9b70bfa50f9dfc10742c79cf4ac1e792b803bb9
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-30 11:33:45 +0100

    Day 6 Part 2 AoC 2016

--- src/main/kotlin/aoc2016/Day6.kt
+++ src/main/kotlin/aoc2016/Day6.kt
@@ -4,6 +4,7 @@ import java.io.File
 
 fun main() {
     Day6().apply { println(part1(File("files/2016/day6.txt").readText())) }
+        .apply { println(part2(File("files/2016/day6.txt").readText())) }
 }
 
 class Day6 {
@@ -18,16 +19,41 @@ class Day6 {
                 }
             }
         var pw = ""
-        for(col in freq) {
+        for (col in freq) {
             var max = 'a'
             var curMax = 0
-            for(entry in col.value) {
-                if(entry.value > curMax) {
+            for (entry in col.value) {
+                if (entry.value > curMax) {
                     max = entry.key
                     curMax = entry.value
                 }
             }
             pw += max
+        }
+        return pw
+    }
+
+    fun part2(input: String): String {
+        val freq = HashMap<Int, HashMap<Char, Int>>()
+        input.lines().map { it.toCharArray() }
+            .forEach {
+                for (i in it.indices) {
+                    freq.computeIfAbsent(i) { HashMap() }
+                    freq[i]!!.computeIfAbsent(it[i]) { 0 }
+                    freq[i]?.set(it[i], freq[i]!![it[i]]!! + 1)
+                }
+            }
+        var pw = ""
+        for (col in freq) {
+            var min = '#'
+            var curMin = Int.MAX_VALUE
+            for (entry in col.value) {
+                if (entry.value < curMin) {
+                    min = entry.key
+                    curMin = entry.value
+                }
+            }
+            pw += min
         }
         return pw
     }
