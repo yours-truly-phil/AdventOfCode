b9b41a939cb9e62aa6ec1ec799e42419aa77332a
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-30 23:19:25 +0100

    day 10 Part 2 AoC 2016

--- src/main/kotlin/aoc2016/Day10.kt
+++ src/main/kotlin/aoc2016/Day10.kt
@@ -4,16 +4,12 @@ import java.io.File
 
 fun main() {
     Day10().apply { println(part1(File("files/2016/day10.txt").readText(), listOf(17, 61))) }
+        .apply { println(part2(File("files/2016/day10.txt").readText())) }
 }
 
 class Day10 {
     fun part1(input: String, containsNums: List<Int>): Int {
-        val instructions = input.lines()
-            .filter { it.startsWith("bot") }
-            .map { Instruction(it) }
-            .map { it.fromId to it }.toMap()
-        val bots = initBots(input.lines().filter { it.startsWith("value") })
-        val outputs = HashMap<Int, ArrayList<Int>>()
+        val (instructions, bots, outputs) = parseInput(input)
 
         while (bots.count { it.value.size == 2 } != 0) {
             bots.filter { it.value.size == 2 }
@@ -37,6 +33,42 @@ class Day10 {
                 }
         }
         return -1
+    }
+
+    private fun parseInput(input: String): Triple<Map<Int, Instruction>, HashMap<Int, ArrayList<Int>>, HashMap<Int, ArrayList<Int>>> {
+        val instructions = input.lines()
+            .filter { it.startsWith("bot") }
+            .map { Instruction(it) }
+            .map { it.fromId to it }.toMap()
+        val bots = initBots(input.lines().filter { it.startsWith("value") })
+        val outputs = HashMap<Int, ArrayList<Int>>()
+        return Triple(instructions, bots, outputs)
+    }
+
+    fun part2(input: String): Int {
+        val (instructions, bots, outputs) = parseInput(input)
+
+        while (bots.count { it.value.size == 2 } != 0) {
+            bots.filter { it.value.size == 2 }
+                .forEach { (k, v) ->
+                    v.sort()
+                    instructions[k]!!.also {
+                        when {
+                            it.lowIsOut -> outputs.computeIfAbsent(it.lowToId) { ArrayList() }
+                                .apply { this += v.removeFirst() }
+                            else -> bots.computeIfAbsent(it.lowToId) { ArrayList() }
+                                .apply { this += v.removeFirst() }
+                        }
+                        when {
+                            it.highIsOut -> outputs.computeIfAbsent(it.highToId) { ArrayList() }
+                                .apply { this += v.removeLast() }
+                            else -> bots.computeIfAbsent(it.highToId) { ArrayList() }
+                                .apply { this += v.removeLast() }
+                        }
+                    }
+                }
+        }
+        return outputs[0]!![0] * outputs[1]!![0] * outputs[2]!![0]
     }
 
     class Instruction(val input: String) {
