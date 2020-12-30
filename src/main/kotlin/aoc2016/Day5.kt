580ccdd8fe5d815051837f33f1c0e92fe96bd2f6
Philipp Seeger <philippbseeger@googlemail.com>
2020-12-30 11:16:35 +0100

    Day 5 Part 2 AoC 2016

--- src/main/kotlin/aoc2016/Day5.kt
+++ src/main/kotlin/aoc2016/Day5.kt
@@ -5,9 +5,8 @@ import java.security.MessageDigest
 
 fun main() {
     Day5()
-        .apply { println(part1("abc")) }
         .apply { println(part1("uqwqemis")) }
-
+        .apply { println(part2("uqwqemis")) }
 }
 
 class Day5 {
@@ -15,7 +14,7 @@ class Day5 {
         var pw = ""
         var i = 0
         while (pw.length < 8) {
-            val md5 = md5(input.plus(i))
+            val md5 = md5f(input.plus(i))
             if (md5.startsWith("00000")) {
                 pw += md5[5]
             }
@@ -24,7 +23,23 @@ class Day5 {
         return pw
     }
 
-    fun md5(input: String): String {
+    fun part2(input: String): String {
+        val pw = HashMap<Int, Char>()
+        var i = 0
+        while (pw.size < 8) {
+            val md5 = md5f(input.plus(i))
+            if (md5.startsWith("00000")) {
+                val pos = md5[5].toInt() - 48
+                if (pos < 8) {
+                    pw.computeIfAbsent(pos) { md5[6] }
+                }
+            }
+            i++
+        }
+        return pw.values.joinToString("")
+    }
+
+    private fun md5f(input: String): String {
         val md = MessageDigest.getInstance("MD5")
         return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
     }
