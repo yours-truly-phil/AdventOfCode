38c022bf325fbe8ad6447d8d5e3be606b4d43207
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-10 11:12:54 +0100

    Day 21 Part 2 AoC 2016 reverse impl

--- src/main/kotlin/aoc2016/Day21.kt
+++ src/main/kotlin/aoc2016/Day21.kt
@@ -35,6 +35,35 @@ class Day21 {
         return pw.joinToString("")
     }
 
+    fun unscramble(s: String, input: String): String {
+        val pw = s.toCharArray()
+        input.lines().reversed().forEach {
+            val parts = it.split(" ")
+            when {
+                parts[0] == "swap" -> {
+                    when {
+                        parts[1] == "position" -> swapPosition(pw, parts[2].toInt(), parts[5].toInt())
+                        parts[1] == "letter" -> swapChars(pw, parts[2][0], parts[5][0])
+                    }
+                }
+                parts[0] == "rotate" -> {
+                    when {
+                        parts[1] == "based" -> rotateBasedReversed(pw, parts[6][0])
+                        parts[1] == "left" -> rotateLeftReversed(pw, parts[2].toInt())
+                        parts[1] == "right" -> rotateRightReversed(pw, parts[2].toInt())
+                    }
+                }
+                parts[0] == "reverse" -> {
+                    reverse(pw, parts[2].toInt(), parts[4].toInt())
+                }
+                parts[0] == "move" -> {
+                    reverseMove(pw, parts[2].toInt(), parts[5].toInt())
+                }
+            }
+        }
+        return pw.joinToString("")
+    }
+
     fun unscrambleBruteforce(s: String, input: String): String {
         val perms = ArrayList<String>()
         perms(s, "", perms)
@@ -59,35 +88,6 @@ class Day21 {
         }
     }
 
-    fun unscramble(s: String, input: String): String {
-        val pw = s.toCharArray()
-        input.lines().reversed().forEach {
-            val parts = it.split(" ")
-            when {
-                parts[0] == "swap" -> {
-                    when {
-                        parts[1] == "position" -> swapPosition(pw, parts[2].toInt(), parts[5].toInt())
-                        parts[1] == "letter" -> swapChars(pw, parts[2][0], parts[5][0])
-                    }
-                }
-                parts[0] == "rotate" -> {
-                    when {
-                        parts[1] == "based" -> rotateBased(pw, parts[6][0]) // TODO reverse
-                        parts[1] == "left" -> rotateRight(pw, parts[2].toInt())
-                        parts[1] == "right" -> rotateLeft(pw, parts[2].toInt())
-                    }
-                }
-                parts[0] == "reverse" -> {
-                    reverse(pw, parts[2].toInt(), parts[4].toInt())
-                }
-                parts[0] == "move" -> {
-                    move(pw, parts[5].toInt(), parts[2].toInt())
-                }
-            }
-        }
-        return pw.joinToString("")
-    }
-
     private fun move(pw: CharArray, i1: Int, i2: Int) {
         if (i1 <= i2) {
             val tmp = pw[i1]
@@ -107,6 +107,10 @@ class Day21 {
         }
     }
 
+    private fun reverseMove(pw: CharArray, i1: Int, i2: Int) {
+        move(pw, i2, i1)
+    }
+
     private fun reverse(pw: CharArray, i1: Int, i2: Int) {
         var num = 0
         while (num <= (i2 - i1) / 2) {
@@ -118,13 +122,28 @@ class Day21 {
     }
 
     private fun rotateBasedReversed(pw: CharArray, c: Char) {
-        // TODO
+        val idx = pw.joinToString("").indexOf(c)
+        if (idx == 1) rotateLeft(pw, 1)
+        if (idx == 3) rotateLeft(pw, 2)
+        if (idx == 5) rotateLeft(pw, 3)
+        if (idx == 7) rotateLeft(pw, 4)
+        if (idx == 2) rotateRight(pw, 2)
+        if (idx == 4) rotateRight(pw, 1)
+        if (idx == 0) rotateLeft(pw, 1)
     }
 
     private fun rotateBased(pw: CharArray, c: Char) {
         var idx = pw.joinToString("").indexOf(c)
         if (idx >= 4) idx++
         rotateRight(pw, idx + 1)
+    }
+
+    private fun rotateRightReversed(pw: CharArray, num: Int) {
+        rotateLeft(pw, num)
+    }
+
+    private fun rotateLeftReversed(pw: CharArray, num: Int) {
+        rotateRight(pw, num)
     }
 
     private fun rotateRight(pw: CharArray, num: Int) {
@@ -164,6 +183,73 @@ class Day21 {
     }
 
     @Test
+    fun `rotate based reversed`() {
+        var arr = "xbcdefgh".toCharArray()
+        rotateBased(arr, 'x')
+        assertEquals("hxbcdefg", arr.joinToString(""))
+        rotateBasedReversed(arr, 'x')
+        assertEquals("xbcdefgh", arr.joinToString(""))
+
+        arr = "axcdefgh".toCharArray()
+        rotateBased(arr, 'x')
+        assertEquals("ghaxcdef", arr.joinToString(""))
+        rotateBasedReversed(arr, 'x')
+        assertEquals("axcdefgh", arr.joinToString(""))
+
+        arr = "abxdefgh".toCharArray()
+        rotateBased(arr, 'x')
+        assertEquals("fghabxde", arr.joinToString(""))
+        rotateBasedReversed(arr, 'x')
+        assertEquals("abxdefgh", arr.joinToString(""))
+
+        arr = "abcxefgh".toCharArray()
+        rotateBased(arr, 'x')
+        assertEquals("efghabcx", arr.joinToString(""))
+        rotateBasedReversed(arr, 'x')
+        assertEquals("abcxefgh", arr.joinToString(""))
+
+        arr = "abcdxfgh".toCharArray()
+        rotateBased(arr, 'x')
+        assertEquals("cdxfghab", arr.joinToString(""))
+        rotateBasedReversed(arr, 'x')
+        assertEquals("abcdxfgh", arr.joinToString(""))
+
+        arr = "abcdexgh".toCharArray()
+        rotateBased(arr, 'x')
+        assertEquals("bcdexgha", arr.joinToString(""))
+        rotateBasedReversed(arr, 'x')
+        assertEquals("abcdexgh", arr.joinToString(""))
+
+        arr = "abcdefxh".toCharArray()
+        rotateBased(arr, 'x')
+        assertEquals("abcdefxh", arr.joinToString(""))
+        rotateBasedReversed(arr, 'x')
+        assertEquals("abcdefxh", arr.joinToString(""))
+
+        arr = "abcdefgx".toCharArray()
+        rotateBased(arr, 'x')
+        assertEquals("xabcdefg", arr.joinToString(""))
+        rotateBasedReversed(arr, 'x')
+        assertEquals("abcdefgx", arr.joinToString(""))
+    }
+
+    @Test
+    fun `rotate right reversed`() {
+        val arr = "abcdefgh".toCharArray()
+        rotateRight(arr, 10)
+        rotateRightReversed(arr, 10)
+        assertEquals("abcdefgh", arr.joinToString(""))
+    }
+
+    @Test
+    fun `rotate left reversed`() {
+        val arr = "abcdefgh".toCharArray()
+        rotateLeft(arr, 10)
+        rotateLeftReversed(arr, 10)
+        assertEquals("abcdefgh", arr.joinToString(""))
+    }
+
+    @Test
     fun `permutations of string`() {
         val perms = ArrayList<String>()
         perms("abcd", "", perms)
@@ -172,6 +258,14 @@ class Day21 {
                 "badc", "bcad", "bcda", "bdac", "bdca", "cabd", "cadb", "cbad",
                 "cbda", "cdab", "cdba", "dabc", "dacb", "dbac", "dbca", "dcab", "dcba")
         ))
+    }
+
+    @Test
+    fun `reverse move`() {
+        val arr = "abcdefgh".toCharArray()
+        move(arr, 1, 5)
+        reverseMove(arr, 1, 5)
+        assertEquals("abcdefgh", arr.joinToString(""))
     }
 
     @Test
@@ -186,6 +280,14 @@ class Day21 {
         val arr = "abcdefg".toCharArray()
         move(arr, 1, 5)
         assertEquals("acdefbg", arr.joinToString(""))
+    }
+
+    @Test
+    fun `reverse reverse`() {
+        val arr = "abcdefgh".toCharArray()
+        reverse(arr, 3, 6)
+        reverse(arr, 3, 6)
+        assertEquals("abcdefgh", arr.joinToString(""))
     }
 
     @Test
@@ -260,6 +362,11 @@ class Day21 {
 
     @Test
     fun part2() {
+        assertEquals("afhdbegc", unscramble("fbgdceah", File("files/2016/day21.txt").readText()))
+    }
+
+    @Test
+    fun `part2 bruteforce`() {
         assertEquals("afhdbegc", unscrambleBruteforce("fbgdceah", File("files/2016/day21.txt").readText()))
     }
 }
