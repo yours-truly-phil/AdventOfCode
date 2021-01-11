15485bcf3735461afda415959395932ae4d2ca6c
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-11 19:16:40 +0100

    Day 1 Part 2 AoC 2017

--- src/main/kotlin/aoc2017/Day1.kt
+++ src/main/kotlin/aoc2017/Day1.kt
@@ -6,10 +6,18 @@ import kotlin.test.assertEquals
 
 class Day1 {
     private fun solveCaptcha(input: String): Int {
-        val nums = input.map { it.toString().toInt() }
-            .toIntArray()
+        val nums = parse(input)
         return nums.indices
             .filter { nums[it] == nums[(it + 1) % nums.size] }
+            .sumBy { nums[it] }
+    }
+
+    private fun parse(input: String) = input.map { it.toString().toInt() }.toIntArray()
+
+    private fun solveNewCaptcha(input: String): Int {
+        val nums = parse(input)
+        return nums.indices
+            .filter { nums[it] == nums[(it + nums.size / 2) % nums.size] }
             .sumBy { nums[it] }
     }
 
@@ -19,6 +27,20 @@ class Day1 {
         assertEquals(4, solveCaptcha("1111"))
         assertEquals(0, solveCaptcha("1234"))
         assertEquals(9, solveCaptcha("91212129"))
+    }
+
+    @Test
+    fun `samples part2`() {
+        assertEquals(6, solveNewCaptcha("1212"))
+        assertEquals(0, solveNewCaptcha("1221"))
+        assertEquals(4, solveNewCaptcha("123425"))
+        assertEquals(12, solveNewCaptcha("123123"))
+        assertEquals(4, solveNewCaptcha("12131415"))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(1292, solveNewCaptcha(File("files/2017/day1.txt").readText()))
     }
 
     @Test
