e15317daa9dacf9d8dcea719512ac6448d611034
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-11 19:32:11 +0100

    Day 2 Part 2 AoC 2017

--- src/main/kotlin/aoc2017/Day2.kt
+++ src/main/kotlin/aoc2017/Day2.kt
@@ -12,6 +12,16 @@ class Day2 {
         }
     }
 
+    fun checksumEvenlyDivisables(input: String): Int {
+        return input.lines()
+            .sumBy { evenlyDivide(it.split("\\s+".toRegex()).map { s -> s.toInt() }.toIntArray()) }
+    }
+
+    fun evenlyDivide(nums: IntArray): Int {
+        nums.forEach { i -> nums.forEach { j -> if (i != j && i % j == 0) return i / j } }
+        throw Exception("no numbers found")
+    }
+
     @Test
     fun sample() {
         assertEquals(18, checksum("5 1 9 5\n" +
@@ -20,7 +30,19 @@ class Day2 {
     }
 
     @Test
+    fun `part2 sample`() {
+        assertEquals(9, checksumEvenlyDivisables("5 9 2 8\n" +
+                "9 4 7 3\n" +
+                "3 8 6 5"))
+    }
+
+    @Test
     fun part1() {
         assertEquals(37923, checksum(File("files/2017/day2.txt").readText()))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(263, checksumEvenlyDivisables(File("files/2017/day2.txt").readText()))
     }
 }
