00258fe709a71906e8b22fcb15c3e5e5695120c7
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-11 23:02:48 +0100

    Day 4 Part 2 AoC 2017

--- src/main/kotlin/aoc2017/Day4.kt
+++ src/main/kotlin/aoc2017/Day4.kt
@@ -26,8 +26,37 @@ class Day4 {
         return count
     }
 
+    fun noPassphraseAddedSecurity(input: String): Int {
+        val passphrases = input.lines().map { it.split(" ") }
+        var count = 0
+        for (phrase in passphrases) {
+            val set = HashSet<String>()
+            var valid = true
+            for (word in phrase) {
+                val sorted = word.toCharArray()
+                sorted.sort()
+                val s = sorted.joinToString("")
+                if (set.contains(s)) {
+                    valid = false
+                    break
+                } else {
+                    set.add(s)
+                }
+            }
+            if (valid) {
+                count++
+            }
+        }
+        return count
+    }
+
     @Test
     fun part1() {
         assertEquals(477, noPassphrases(File("files/2017/day4.txt").readText()))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(167, noPassphraseAddedSecurity(File("files/2017/day4.txt").readText()))
     }
 }
