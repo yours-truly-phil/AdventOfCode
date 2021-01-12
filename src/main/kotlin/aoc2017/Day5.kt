dcd4684d871a129795cb29f961bb7822deae212b
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-12 18:18:50 +0100

    Day 5 Part 2 AoC 2017

--- src/main/kotlin/aoc2017/Day5.kt
+++ src/main/kotlin/aoc2017/Day5.kt
@@ -18,6 +18,22 @@ class Day5 {
         return steps
     }
 
+    fun countStepsStrangerToExit(input: String):Int {
+        val instructions = input.lines().map { it.toInt() }.toIntArray()
+        var idx = 0
+        var steps = 0
+        while (idx >= 0 && idx < instructions.size) {
+            val inst = instructions[idx]
+            when {
+                inst >= 3 -> instructions[idx]--
+                else -> instructions[idx]++
+            }
+            idx += inst
+            steps++
+        }
+        return steps
+    }
+
     @Test
     fun sample() {
         assertEquals(5, countStepsToExit("0\n" +
@@ -28,7 +44,21 @@ class Day5 {
     }
 
     @Test
+    fun `part2 sample`() {
+        assertEquals(10, countStepsStrangerToExit("0\n" +
+                "3\n" +
+                "0\n" +
+                "1\n" +
+                "-3"))
+    }
+
+    @Test
     fun part1() {
         assertEquals(318883, countStepsToExit(File("files/2017/day5.txt").readText()))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(23948711, countStepsStrangerToExit(File("files/2017/day5.txt").readText()))
     }
 }
