187878bcfcf17a556b047faf51548cfc17944a0c
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-12 23:56:02 +0100

    Day 8 Part 1 AoC 2017

---
+++ src/main/kotlin/aoc2017/Day8.kt
@@ -0,0 +1,54 @@
+package aoc2017
+
+import org.junit.jupiter.api.Test
+import java.io.File
+import kotlin.test.assertEquals
+
+class Day8 {
+    fun largestValue(input: String): Int {
+        val mem = HashMap<String, Int>()
+        input.lines().forEach {
+            val parts = it.split(" if ")
+            val left = parts[0].split(" ")
+            val reg = left[0]
+            val inst = left[1]
+            val num = left[2].toInt()
+            val cond = parts[1].split(" ")
+            val cReg = cond[0]
+            val op = cond[1]
+            val cVal = cond[2].toInt()
+
+            mem.computeIfAbsent(reg) { 0 }
+            mem.computeIfAbsent(cReg) { 0 }
+            var condRes = false
+            when (op) {
+                ">" -> condRes = mem[cReg]!! > cVal
+                "<" -> condRes = mem[cReg]!! < cVal
+                ">=" -> condRes = mem[cReg]!! >= cVal
+                "<=" -> condRes = mem[cReg]!! <= cVal
+                "==" -> condRes = mem[cReg]!! == cVal
+                "!=" -> condRes = mem[cReg]!! != cVal
+            }
+            if (condRes) {
+                when (inst) {
+                    "inc" -> mem[reg] = mem[reg]!! + num
+                    "dec" -> mem[reg] = mem[reg]!! - num
+                }
+            }
+        }
+        return mem.maxOf { it.value }
+    }
+
+    @Test
+    fun sample() {
+        assertEquals(1, largestValue("b inc 5 if a > 1\n" +
+                "a inc 1 if b < 5\n" +
+                "c dec -10 if a >= 1\n" +
+                "c inc -20 if c == 10"))
+    }
+
+    @Test
+    fun part1() {
+        assertEquals(5221, largestValue(File("files/2017/day8.txt").readText()))
+    }
+}
