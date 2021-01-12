bc962c787c3ff2f170628aac269114608434fcf2
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-13 00:01:53 +0100

    Day 8 Part 2 AoC 2017

--- src/main/kotlin/aoc2017/Day8.kt
+++ src/main/kotlin/aoc2017/Day8.kt
@@ -39,6 +39,50 @@ class Day8 {
         return mem.maxOf { it.value }
     }
 
+    private fun largestValueDuringRuntime(input: String): Int {
+        val mem = HashMap<String, Int>()
+        var max = Int.MIN_VALUE
+        input.lines().forEach {
+            val parts = it.split(" if ")
+            val left = parts[0].split(" ")
+            val reg = left[0]
+            val inst = left[1]
+            val num = left[2].toInt()
+            val cond = parts[1].split(" ")
+            val cReg = cond[0]
+            val op = cond[1]
+            val cVal = cond[2].toInt()
+
+            mem.computeIfAbsent(reg) { 0 }
+            mem.computeIfAbsent(cReg) { 0 }
+            var condRes = false
+            when (op) {
+                ">" -> condRes = mem[cReg]!! > cVal
+                "<" -> condRes = mem[cReg]!! < cVal
+                ">=" -> condRes = mem[cReg]!! >= cVal
+                "<=" -> condRes = mem[cReg]!! <= cVal
+                "==" -> condRes = mem[cReg]!! == cVal
+                "!=" -> condRes = mem[cReg]!! != cVal
+            }
+            if (condRes) {
+                when (inst) {
+                    "inc" -> mem[reg] = mem[reg]!! + num
+                    "dec" -> mem[reg] = mem[reg]!! - num
+                }
+            }
+            max = maxOf(max, mem.maxOf { m -> m.value })
+        }
+        return max
+    }
+
+    @Test
+    fun `part 2 sample`() {
+        assertEquals(10, largestValueDuringRuntime("b inc 5 if a > 1\n" +
+                "a inc 1 if b < 5\n" +
+                "c dec -10 if a >= 1\n" +
+                "c inc -20 if c == 10"))
+    }
+
     @Test
     fun sample() {
         assertEquals(1, largestValue("b inc 5 if a > 1\n" +
@@ -50,5 +94,10 @@ class Day8 {
     @Test
     fun part1() {
         assertEquals(5221, largestValue(File("files/2017/day8.txt").readText()))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(7491, largestValueDuringRuntime(File("files/2017/day8.txt").readText()))
     }
 }
