8a06010dc1f084ce61f4d634b0ca8d7c85b02719
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-12 18:15:17 +0100

    Day 5 Part 1 AoC 2017

---
+++ src/main/kotlin/aoc2017/Day5.kt
@@ -0,0 +1,34 @@
+package aoc2017
+
+import org.junit.jupiter.api.Test
+import java.io.File
+import kotlin.test.assertEquals
+
+class Day5 {
+    fun countStepsToExit(input: String): Int {
+        val instructions = input.lines().map { it.toInt() }.toIntArray()
+        var idx = 0
+        var steps = 0
+        while (idx >= 0 && idx < instructions.size) {
+            val inst = instructions[idx]
+            instructions[idx]++
+            idx += inst
+            steps++
+        }
+        return steps
+    }
+
+    @Test
+    fun sample() {
+        assertEquals(5, countStepsToExit("0\n" +
+                "3\n" +
+                "0\n" +
+                "1\n" +
+                "-3"))
+    }
+
+    @Test
+    fun part1() {
+        assertEquals(318883, countStepsToExit(File("files/2017/day5.txt").readText()))
+    }
+}
