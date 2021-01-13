897d03988fba40a1277b25c5e731e825a32b1fcc
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-13 19:25:38 +0100

    Day 11 Part 2 AoC 2017

--- src/main/kotlin/aoc2017/Day11.kt
+++ src/main/kotlin/aoc2017/Day11.kt
@@ -27,8 +27,36 @@ class Day11 {
         return (abs(x) + abs(y)) / 2
     }
 
+    fun maxDist(input: String): Int {
+        val path = input.split(",").map {
+            when (it) {
+                "n" -> Pair(2, 0)
+                "ne" -> Pair(1, 1)
+                "se" -> Pair(-1, 1)
+                "s" -> Pair(-2, 0)
+                "sw" -> Pair(-1, -1)
+                else -> Pair(1, -1)
+            }
+        }
+        var x = 0
+        var max = 0
+        var y = 0
+        path.forEach {
+            x += it.first
+            y += it.second
+            max = maxOf(max, (abs(x) + abs(y)) / 2)
+        }
+
+        return max
+    }
+
     @Test
     fun part1() {
         assertEquals(810, minSteps(File("files/2017/day11.txt").readText()))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(1567, maxDist(File("files/2017/day11.txt").readText()))
     }
 }
