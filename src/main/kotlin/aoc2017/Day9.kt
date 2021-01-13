6daa6100056ccac0e0a760f66f0bf9da87968877
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-13 17:08:01 +0100

    Day 9 Part 1 AoC 2017

---
+++ src/main/kotlin/aoc2017/Day9.kt
@@ -0,0 +1,108 @@
+package aoc2017
+
+import org.junit.jupiter.api.Test
+import java.io.File
+import kotlin.test.assertEquals
+
+class Day9 {
+    private fun totalScore(input: String): Int {
+        val cleanedUp = cleanup(input)
+        val count = countGroups(cleanedUp)
+        var total = 0
+        count.forEach { total += it.key * it.value }
+        return total
+    }
+
+    private fun countGroups(input: String): HashMap<Int, Int> {
+        val counts = HashMap<Int, Int>()
+        var lvl = 0
+        for (c in input) {
+            if (c == '{') {
+                lvl++
+                counts.computeIfAbsent(lvl) { 0 }
+                counts[lvl] = counts[lvl]!! + 1
+            } else if (c == '}') {
+                lvl--
+            }
+        }
+        return counts
+    }
+
+    private fun cleanup(input: String): String {
+        return removeTrash(removeExclamation(removeIgnoredChars(input)))
+    }
+
+    private fun removeTrash(input: String): String {
+        val sb = StringBuilder()
+        var ignore = false
+        for (c in input) {
+            when {
+                c == '<' && !ignore -> ignore = true
+                c == '>' && ignore -> ignore = false
+                else -> if (!ignore) sb.append(c)
+            }
+        }
+        return sb.toString()
+    }
+
+    private fun removeIgnoredChars(input: String): String {
+        val list = input.toMutableList()
+        var i = 0
+        while (i < list.size - 1) {
+            if (list[i] == '!') {
+                list.removeAt(i + 1)
+            }
+            i++
+        }
+        return list.joinToString("")
+    }
+
+    private fun removeExclamation(input: String): String {
+        return input.replace("!", "")
+    }
+
+    @Test
+    fun `count groups`() {
+        val counts = countGroups("{{{},{},{{}}}}")
+        assertEquals(1, counts[1])
+        assertEquals(1, counts[2])
+        assertEquals(3, counts[3])
+        assertEquals(1, counts[4])
+    }
+
+    @Test
+    fun `cleanup trash`() {
+        assertEquals("", cleanup("<{o\"i!a,<{i<a>"))
+        assertEquals("", cleanup("<random characters>"))
+        assertEquals("{{},{},{},{}}", cleanup("{{<ab>},{<ab>},{<ab>},{<ab>}}"))
+    }
+
+    @Test
+    fun `remove ignored chars`() {
+        assertEquals("<!!>", removeIgnoredChars("<!!!>>"))
+        assertEquals("<!>", removeIgnoredChars("<!!>"))
+    }
+
+    @Test
+    fun `remove exclamation mark`() {
+        assertEquals("<>", removeExclamation("<!>"))
+        assertEquals("<>", removeExclamation("<!!>"))
+    }
+
+    @Test
+    fun `calculate score`() {
+        assertEquals(1, totalScore("{}"))
+        assertEquals(6, totalScore("{{{}}}"))
+        assertEquals(5, totalScore("{{},{}}"))
+        assertEquals(16, totalScore("{{{},{},{{}}}}"))
+        assertEquals(1, totalScore("{<a>,<a>,<a>,<a>}"))
+        assertEquals(9, totalScore("{{<ab>},{<ab>},{<ab>},{<ab>}}"))
+        assertEquals(9, totalScore("{{<!!>},{<!!>},{<!!>},{<!!>}}"))
+        assertEquals(3, totalScore("{{<a!>},{<a!>},{<a!>},{<ab>}}"))
+    }
+
+    @Test
+    fun part1() {
+        assertEquals(10820, totalScore(File("files/2017/day9.txt").readText()))
+    }
+}
