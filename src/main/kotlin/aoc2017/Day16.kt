e319c13bef27a3b54731363bfd5d8c6c251b5840
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-14 20:38:59 +0100

    Day 16 Part 2 AoC 2017

--- src/main/kotlin/aoc2017/Day16.kt
+++ src/main/kotlin/aoc2017/Day16.kt
@@ -2,6 +2,7 @@ package aoc2017
 
 import org.junit.jupiter.api.Test
 import java.io.File
+import java.util.function.Consumer
 import kotlin.test.assertEquals
 
 class Day16 {
@@ -17,32 +18,113 @@ class Day16 {
         return mem.joinToString("")
     }
 
+    fun instructions(input: String): Array<Consumer<CharArray>> {
+        val res = ArrayList<Consumer<CharArray>>()
+        input.split(",").forEach {
+            when {
+                it.startsWith("s") -> {
+                    res += Consumer<CharArray> { arr -> spinByAmount(it.substring(1).toInt(), arr) }
+                }
+                it.startsWith("x") -> {
+                    val parts = it.substring(1).split("/")
+                    val a = parts[0].toInt()
+                    val b = parts[1].toInt()
+                    res += Consumer<CharArray> { arr -> swap(arr, a, b) }
+                }
+                it.startsWith("p") -> {
+                    val parts = it.substring(1).split("/")
+                    res += Consumer<CharArray> { arr ->
+                        run {
+                            val a = arr.indexOf(parts[0].first())
+                            val b = arr.indexOf(parts[1].first())
+                            swap(arr, a, b)
+                        }
+                    }
+                }
+            }
+        }
+        return res.toTypedArray()
+    }
+
+    fun part2RepeatDancesWithParsing(num: Int, letters: String, input: String): String {
+        var order = letters
+        val memo = HashMap<String, Int>()
+        var idx = 0
+        while (idx < num) {
+            memo.computeIfAbsent(order) { idx }
+            order = orderOfPrograms(order, input)
+            idx++
+            if (memo.containsKey(order)) {
+                val diff = idx - memo[order]!!
+                while (idx < num - diff) {
+                    idx += diff
+                }
+            }
+        }
+        return order
+    }
+
+    fun part2WithoutRepeatedParsing(num: Int, letters: String, input: String): String {
+        val instructions = instructions(input)
+        val memo = HashMap<String, Int>()
+        val order = letters.toCharArray()
+        var idx = 0
+        while (idx < num) {
+            memo.computeIfAbsent(order.joinToString("")) { idx }
+            for (instruction in instructions) {
+                instruction.accept(order)
+            }
+            idx++
+            if (memo.containsKey(order.joinToString(""))) {
+                val diff = idx - memo[order.joinToString("")]!!
+                while (idx < num - diff) {
+                    idx += diff
+                }
+            }
+        }
+        return order.joinToString("")
+    }
+
     private fun partner(it: String, mem: CharArray) {
         val parts = it.substring(1).split("/")
         val a = mem.indexOf(parts[0].first())
         val b = mem.indexOf(parts[1].first())
-        val tmp = mem[a]
-        mem[a] = mem[b]
-        mem[b] = tmp
+        swap(mem, a, b)
     }
 
     private fun exchange(it: String, mem: CharArray) {
         val parts = it.substring(1).split("/")
         val a = parts[0].toInt()
         val b = parts[1].toInt()
+        swap(mem, a, b)
+    }
+
+    private fun swap(mem: CharArray, a: Int, b: Int) {
         val tmp = mem[a]
         mem[a] = mem[b]
         mem[b] = tmp
     }
 
-    private fun spin(it: String, arr: CharArray) {
-        repeat(it.substring(1).toInt()) {
-            val tmp = arr.last()
-            for (i in arr.size - 1 downTo 1) {
-                arr[i] = arr[i - 1]
-            }
-            arr[0] = tmp
+    private fun spinByAmount(num: Int, arr: CharArray) {
+        val tmp = arr.clone()
+        for (i in arr.indices) {
+            tmp[i] = arr[(i + arr.size - num) % arr.size]
         }
+        for (i in arr.indices) {
+            arr[i] = tmp[i]
+        }
+    }
+
+    private fun spin(it: String, arr: CharArray) {
+        val num = it.substring(1).toInt()
+        spinByAmount(num, arr)
+    }
+
+    @Test
+    fun `spin by amount`() {
+        val arr = "abcde".toCharArray()
+        spinByAmount(3, arr)
+        assertEquals("cdeab", arr.joinToString(""))
     }
 
     @Test
@@ -78,5 +160,17 @@ class Day16 {
     fun part1() {
         assertEquals("cknmidebghlajpfo",
             orderOfPrograms("abcdefghijklmnop", File("files/2017/day16.txt").readText()))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals("cbolhmkgfpenidaj",
+            part2WithoutRepeatedParsing(1_000_000_000, "abcdefghijklmnop", File("files/2017/day16.txt").readText()))
+    }
+
+    @Test
+    fun part2RepeatWithParsing() {
+        assertEquals("cbolhmkgfpenidaj",
+            part2RepeatDancesWithParsing(1_000_000_000, "abcdefghijklmnop", File("files/2017/day16.txt").readText()))
     }
 }
