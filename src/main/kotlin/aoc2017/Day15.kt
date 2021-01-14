713bd7bbd2a6c1c7cfd93874bfc1abbc13b02440
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-14 19:24:16 +0100

    Day 15 Part 2 AoC 2017

--- src/main/kotlin/aoc2017/Day15.kt
+++ src/main/kotlin/aoc2017/Day15.kt
@@ -16,13 +16,37 @@ class Day15 {
         return count
     }
 
+    fun finalCountPart2(genA: Long, genB: Long): Int {
+        var a = genA
+        var b = genB
+        var count = 0
+        repeat(5_000_000) {
+            a = (a * 16807) % 2147483647
+            b = (b * 48271) % 2147483647
+            while (a % 4 != 0L) a = (a * 16807) % 2147483647
+            while (b % 8 != 0L) b = (b * 48271) % 2147483647
+            if (a and 0xffff == b and 0xffff) count++
+        }
+        return count
+    }
+
     @Test
     fun sample() {
         assertEquals(588, finalCount(65, 8921))
     }
 
     @Test
+    fun `part 2 sample`() {
+        assertEquals(309, finalCountPart2(65, 8921))
+    }
+
+    @Test
     fun part1() {
         assertEquals(619, finalCount(591, 393))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(290, finalCountPart2(591, 393))
     }
 }
