5fc0b3f08e830271f35051f1939e2ec2fd6b465c
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-15 17:59:06 +0100

    Day 17 Part 1 AoC 2017

--- src/main/kotlin/aoc2017/Day17.kt
+++ src/main/kotlin/aoc2017/Day17.kt
@@ -1,15 +1,27 @@
 package aoc2017
 
 import org.junit.jupiter.api.Test
+import java.util.*
 import kotlin.test.assertEquals
 
 class Day17 {
-    fun circularBufVal(i: Int): Int {
-        return -1
+    fun circularBufVal(steps: Int): Int {
+        var idx = 0
+        val buf = LinkedList<Int>().apply { add(0) }
+        repeat(2017) {
+            idx = (idx + steps) % buf.size + 1
+            buf.add(idx, it + 1)
+        }
+        return buf[(idx + 1) % buf.size]
+    }
+
+    @Test
+    fun sample() {
+        assertEquals(638, circularBufVal(3))
     }
 
     @Test
     fun part1() {
-        assertEquals(-1, circularBufVal(386))
+        assertEquals(419, circularBufVal(386))
     }
 }
