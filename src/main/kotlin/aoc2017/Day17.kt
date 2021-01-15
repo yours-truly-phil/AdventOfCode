935a709fe7d5098d7f9b086d5fb4686bf29892b9
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-15 18:04:31 +0100

    Day 17 Part 2 AoC 2017

--- src/main/kotlin/aoc2017/Day17.kt
+++ src/main/kotlin/aoc2017/Day17.kt
@@ -15,6 +15,20 @@ class Day17 {
         return buf[(idx + 1) % buf.size]
     }
 
+    fun valueAfterZero(steps: Int): Int {
+        var idx = 0
+        var size = 1
+        var idxZero = 0
+        var numAfterZero = 0
+        repeat(50_000_000) {
+            idx = (idx + steps) % size + 1
+            if (idx <= idxZero) idxZero++
+            else if (idx == idxZero + 1) numAfterZero = it + 1
+            size++
+        }
+        return numAfterZero
+    }
+
     @Test
     fun sample() {
         assertEquals(638, circularBufVal(3))
@@ -23,5 +37,10 @@ class Day17 {
     @Test
     fun part1() {
         assertEquals(419, circularBufVal(386))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(46038988, valueAfterZero(386))
     }
 }
