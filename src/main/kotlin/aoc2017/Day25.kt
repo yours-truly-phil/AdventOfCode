b86dc5547a4335d6f131890b4cb60f92480d2572
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-16 23:35:10 +0100

    Day 25 Part 1 AoC 2017

---
+++ src/main/kotlin/aoc2017/Day25.kt
@@ -0,0 +1,82 @@
+package aoc2017
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day25 {
+    fun diagnosticChecksum(steps: Int): Int {
+        val reg = HashMap<Int, Boolean>()
+        var v = 'a'
+        var i = 0
+        repeat(steps) {
+            reg.computeIfAbsent(i) { false }
+            if (v == 'a') {
+                if (!reg[i]!!) {
+                    reg[i] = true
+                    i++
+                    v = 'b'
+                } else {
+                    reg[i] = false
+                    i--
+                    v = 'b'
+                }
+            } else if (v == 'b') {
+                if (!reg[i]!!) {
+                    reg[i] = false
+                    i++
+                    v = 'c'
+                } else {
+                    reg[i] = true
+                    i--
+                    v = 'b'
+                }
+            } else if (v == 'c') {
+                if (!reg[i]!!) {
+                    reg[i] = true
+                    i++
+                    v = 'd'
+                } else {
+                    reg[i] = false
+                    i--
+                    v = 'a'
+                }
+            } else if (v == 'd') {
+                if (!reg[i]!!) {
+                    reg[i] = true
+                    i--
+                    v = 'e'
+                } else {
+                    reg[i] = true
+                    i--
+                    v = 'f'
+                }
+            } else if (v == 'e') {
+                if (!reg[i]!!) {
+                    reg[i] = true
+                    i--
+                    v = 'a'
+                } else {
+                    reg[i] = false
+                    i--
+                    v = 'd'
+                }
+            } else if (v == 'f') {
+                if (!reg[i]!!) {
+                    reg[i] = true
+                    i++
+                    v = 'a'
+                } else {
+                    reg[i] = true
+                    i--
+                    v = 'e'
+                }
+            }
+        }
+        return reg.filter { it.value }.count()
+    }
+
+    @Test
+    fun part1() {
+        assertEquals(3732, diagnosticChecksum(12586542))
+    }
+}
