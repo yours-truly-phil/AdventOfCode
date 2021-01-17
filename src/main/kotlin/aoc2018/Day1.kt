61d9b2176940a13349225db2036e8d6f9e75507b
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-17 21:47:55 +0100

    Day 1 Part 2 AoC 2018

--- src/main/kotlin/aoc2018/Day1.kt
+++ src/main/kotlin/aoc2018/Day1.kt
@@ -7,8 +7,34 @@ import kotlin.test.assertEquals
 class Day1 {
     fun resultingFrequency(input: String): Int = input.lines().map { it.toInt() }.sum()
 
+    fun firstRepeatedFrequency(input: String): Int {
+        val nums = input.lines().map { it.toInt() }.toIntArray()
+        val memo = HashSet<Int>()
+        var sum = 0
+        while (true) {
+            for (num in nums) {
+                if (memo.contains(sum)) {
+                    return sum
+                }
+                memo.add(sum)
+                sum += num
+            }
+        }
+    }
+
+    @Test
+    fun `part 2 sample`() {
+        assertEquals(0, firstRepeatedFrequency("+1\n-1"))
+        assertEquals(10, firstRepeatedFrequency("+3\n+3\n+4\n-2\n-4"))
+    }
+
     @Test
     fun part1() {
         assertEquals(536, resultingFrequency(File("files/2018/day1.txt").readText()))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(75108, firstRepeatedFrequency(File("files/2018/day1.txt").readText()))
     }
 }
