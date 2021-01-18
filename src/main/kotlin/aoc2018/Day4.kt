8e20af4aa139b87173b55dbabec02d45da71e08f
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-18 23:02:26 +0100

    Day 4 Part 2 AoC 2018

--- src/main/kotlin/aoc2018/Day4.kt
+++ src/main/kotlin/aoc2018/Day4.kt
@@ -42,6 +42,45 @@ class Day4 {
         return maxSleepGuard.key * minutesAsleep.indexOf(minutesAsleep.maxOf { it })
     }
 
+    fun guardMostFrequentlyAsleep(input: String):Int {
+        val dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+        val lines = input.lines()
+            .map {
+                LocalDateTime.parse(it.substring(it.indexOf("[") + 1, it.indexOf("]")), dtf) to it
+            }.sortedBy { it.first }
+
+        val sleeps = HashMap<Int, MutableList<Sleep>>()
+        var curGuard = 0
+        var curSleep = Sleep(-1, -1, -1)
+        lines.forEach {
+            when {
+                it.second.contains("Guard") -> {
+                    curGuard = it.second.split("#")[1].split(" ")[0].toInt()
+                    sleeps.computeIfAbsent(curGuard) { ArrayList() }
+                }
+                it.second.contains("falls") -> {
+                    curSleep = Sleep(curGuard, it.first.dayOfYear, it.first.minute)
+                        .also { sleep -> sleeps[curGuard]!!.add(sleep) }
+                }
+                it.second.contains("wakes") ->
+                    curSleep.to = it.first.minute
+            }
+        }
+
+
+        val sleepMap = HashMap<Int, IntArray>()
+        sleeps.forEach { t, u ->
+            sleepMap.computeIfAbsent(t) { IntArray(60) }
+            u.forEach {
+                for(i in it.from until it.to) {
+                    sleepMap[t]!![i] = sleepMap[t]!![i] + 1
+                }
+            }
+        }
+        val maxSleep = sleepMap.maxByOrNull { it.value.maxOf { i -> i } }!!
+        return maxSleep.key * maxSleep.value.indexOf(maxSleep.value.maxOf { it })
+    }
+
     class Sleep(val guard: Int, val dayOfYear: Int, val from: Int) {
         var to: Int = -1
 
@@ -69,6 +108,32 @@ class Day4 {
                 "[1518-11-05 00:03] Guard #99 begins shift\n" +
                 "[1518-11-05 00:45] falls asleep\n" +
                 "[1518-11-05 00:55] wakes up"))
+    }
+
+    @Test
+    fun `part 2 sample`() {
+        assertEquals(4455, guardMostFrequentlyAsleep("[1518-11-01 00:00] Guard #10 begins shift\n" +
+                "[1518-11-01 00:05] falls asleep\n" +
+                "[1518-11-01 00:25] wakes up\n" +
+                "[1518-11-01 00:30] falls asleep\n" +
+                "[1518-11-01 00:55] wakes up\n" +
+                "[1518-11-01 23:58] Guard #99 begins shift\n" +
+                "[1518-11-02 00:40] falls asleep\n" +
+                "[1518-11-02 00:50] wakes up\n" +
+                "[1518-11-03 00:05] Guard #10 begins shift\n" +
+                "[1518-11-03 00:24] falls asleep\n" +
+                "[1518-11-03 00:29] wakes up\n" +
+                "[1518-11-04 00:02] Guard #99 begins shift\n" +
+                "[1518-11-04 00:36] falls asleep\n" +
+                "[1518-11-04 00:46] wakes up\n" +
+                "[1518-11-05 00:03] Guard #99 begins shift\n" +
+                "[1518-11-05 00:45] falls asleep\n" +
+                "[1518-11-05 00:55] wakes up"))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(65854, guardMostFrequentlyAsleep(File("files/2018/day4.txt").readText()))
     }
 
     @Test
