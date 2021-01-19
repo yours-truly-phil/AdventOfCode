0a3259225c77c7cf9f7db7a0a2129ad2d3dc4b53
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-19 22:32:28 +0100

    Day 8 Part 2 AoC 2018

--- src/main/kotlin/aoc2018/Day8.kt
+++ src/main/kotlin/aoc2018/Day8.kt
@@ -6,12 +6,18 @@ import kotlin.test.assertEquals
 
 class Day8 {
     private fun sumMetaData(input: String): Int {
+        val root = parseRoot(input)
+        return sumMeta(root)
+    }
+
+    private fun parseRoot(input: String): Node {
         val nums = input.split(" ").map { it.toInt() }.toIntArray()
         val master = Node()
         parseNodes(0, nums, master)
-        val root = master.children.first()
-        return sumMeta(root)
+        return master.children.first()
     }
+
+    private fun valueOfRootNode(input: String): Int = parseRoot(input).nodeValue()
 
     private fun sumMeta(node: Node): Int {
         return node.meta.sum() + node.children.map { sumMeta(it) }.sum()
@@ -33,6 +39,22 @@ class Day8 {
     class Node {
         val children = ArrayList<Node>()
         val meta = ArrayList<Int>()
+
+        fun nodeValue(): Int {
+            return if (children.isEmpty()) {
+                meta.sum()
+            } else {
+                var value = 0
+                for (i in meta) {
+                    val idx = i - 1
+                    if (idx in children.indices) {
+                        value += children[idx].nodeValue()
+                    }
+                }
+                value
+            }
+        }
+
         override fun toString(): String {
             return "N(c=$children, m=$meta)"
         }
@@ -44,7 +66,17 @@ class Day8 {
     }
 
     @Test
+    fun `part 2 sample`() {
+        assertEquals(66, valueOfRootNode("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"))
+    }
+
+    @Test
     fun part1() {
         assertEquals(38780, sumMetaData(File("files/2018/day8.txt").readText()))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(18232, valueOfRootNode(File("files/2018/day8.txt").readText()))
     }
 }
