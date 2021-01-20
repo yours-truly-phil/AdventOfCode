6cdb2b8f95fe33ca5ea4ad8e533a8037062373d5
Philipp Seeger <philippbseeger@googlemail.com>
2021-01-20 20:06:39 +0100

    Day 9 Part 2 AoC 2018

--- src/main/kotlin/aoc2018/Day9.kt
+++ src/main/kotlin/aoc2018/Day9.kt
@@ -1,20 +1,23 @@
 package aoc2018
 
 import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.collections.ArrayDeque
 import kotlin.test.assertEquals
 
 class Day9 {
-    fun scoreOfWinner(noPlayers: Int, valueLastMarble: Int): Int {
-        val scores = IntArray(noPlayers)
+    fun scoreOfWinner(noPlayers: Int, valueLastMarble: Int): Long {
+        val start = System.currentTimeMillis()
+        val scores = LongArray(noPlayers)
         var idx = 0
         var curPlayer = 0
         val marbles = ArrayDeque<Int>().also { it.add(0) }
         for (i in 1..valueLastMarble) {
             if (i % 23 == 0) {
-                scores[curPlayer] += i
+                scores[curPlayer] += i.toLong()
                 idx += (marbles.size - 7)
                 idx %= marbles.size
-                scores[curPlayer] += marbles.removeAt(idx)
+                scores[curPlayer] += marbles.removeAt(idx).toLong()
             } else {
                 idx += 2
                 idx %= marbles.size
@@ -22,7 +25,14 @@ class Day9 {
             }
             curPlayer++
             curPlayer %= scores.size
+
+            // maybe just rotate if idx gets too big
+            if (idx > 150_000) {
+                Collections.rotate(marbles, -idx)
+                idx = 0
+            }
         }
+        println("${System.currentTimeMillis() - start}ms")
         return scores.maxOf { it }
     }
 
@@ -39,5 +49,10 @@ class Day9 {
     @Test
     fun part1() {
         assertEquals(388131, scoreOfWinner(459, 72103))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(3239376988, scoreOfWinner(459, 72103 * 100))
     }
 }
