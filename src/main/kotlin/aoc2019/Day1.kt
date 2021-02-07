633d2ec27b6ef38e36f636451591951b2507e939
Philipp Seeger <pseeger@k15t.com>
2021-02-07 19:49:23 +0100

    AoC 2019 Day 1 Part 2

--- src/main/kotlin/aoc2019/Day1.kt
+++ src/main/kotlin/aoc2019/Day1.kt
@@ -10,8 +10,30 @@ class Day1 {
             .map { it.toInt() }
             .map { it / 3 - 2 }.sum()
 
+    fun fuelReqsPart2(input: String): Int {
+        return input.lines()
+            .map { it.toInt() }
+            .map { necessaryFuel(it) }
+            .sum()
+    }
+
+    fun necessaryFuel(mass: Int):Int {
+        var count = 0
+        var cur = mass
+        while(cur > 0) {
+            cur = cur / 3 - 2
+            if(cur > 0) count += cur
+        }
+        return count
+    }
+
     @Test
     fun part1() {
         assertEquals(3497399, sumFuelRequirements(File("files/2019/day1.txt").readText()))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(5243207, fuelReqsPart2(File("files/2019/day1.txt").readText()))
     }
 }
