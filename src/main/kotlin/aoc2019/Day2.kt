3739f7c43567ca67ca21bc70f8933a4790df76b8
Philipp Seeger <pseeger@k15t.com>
2021-02-07 20:13:00 +0100

    AoC 2019 Day 2 Part 1

---
+++ src/main/kotlin/aoc2019/Day2.kt
@@ -0,0 +1,40 @@
+package aoc2019
+
+import org.junit.jupiter.api.Test
+import java.io.File
+import java.util.function.Consumer
+import kotlin.test.assertEquals
+
+class Day2 {
+    fun valueInPos0(input: String, transform: Consumer<IntArray>): Int {
+        val arr = input.split(",").map { it.toInt() }.toIntArray()
+        transform.accept(arr)
+        for (i in arr.indices step 4) {
+            when (arr[i]) {
+                1 -> arr[arr[i + 3]] = arr[arr[i + 1]] + arr[arr[i + 2]]
+                2 -> arr[arr[i + 3]] = arr[arr[i + 1]] * arr[arr[i + 2]]
+                99 -> return arr[0]
+            }
+        }
+        return -1
+    }
+
+    @Test
+    fun samples() {
+        assertEquals(2, valueInPos0("1,0,0,0,99") {})
+        assertEquals(2, valueInPos0("2,3,0,3,99") {})
+        assertEquals(2, valueInPos0("2,4,4,5,99,0") {})
+        assertEquals(30, valueInPos0("1,1,1,4,99,5,6,0,99") {})
+    }
+
+    @Test
+    fun part1() {
+        assertEquals(2842648, valueInPos0(File("files/2019/day2.txt").readText())
+        { arr ->
+            run {
+                arr[1] = 12
+                arr[2] = 2
+            }
+        })
+    }
+}
