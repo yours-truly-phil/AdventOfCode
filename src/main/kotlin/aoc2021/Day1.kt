cc741ed61ce0cae544b9714d1f9639f554df6479
Philipp Seeger <pseeger@k15t.com>
2021-12-01 09:02:03 +0100

    AoC 2021 Day 1

--- src/main/kotlin/aoc2021/Day1.kt
+++ src/main/kotlin/aoc2021/Day1.kt
@@ -5,21 +5,45 @@ import java.io.File
 import kotlin.test.assertEquals
 
 class Day1 {
-    private fun foo(input: String): Int {
-        return -1
+    private fun numTimesDepthIncreases(input: String): Int {
+        val nums = parseInput(input)
+        var res = 0
+        for (i in 1 until nums.size) {
+            if (nums[i] > nums[i - 1]) {
+                res++
+            }
+        }
+        return res
     }
 
-    private fun bar(input: String): Int {
-        return -1
+    private fun numTimesSumTripletsIncreases(input: String): Int {
+        val nums = parseInput(input)
+        var res = 0
+        for (i in 1 until nums.size - 2) {
+            val first = nums[i - 1] + nums[i] + nums[i + 1]
+            val second = nums[i] + nums[i + 1] + nums[i + 2]
+            if (first < second) {
+                res++
+            }
+        }
+        return res
+    }
+
+    private fun parseInput(input: String) =
+        input.lines().filter { it.isNotEmpty() }.map { it.toInt() }
+
+    @Test
+    fun numTimesDepthIncreases() {
+        assertEquals(7, numTimesDepthIncreases("199\n200\n208\n210\n200\n207\n240\n269\n260\n263"))
+        assertEquals(
+            1791,
+            numTimesDepthIncreases(File("files/2021/day1.txt").readText())
+        )
     }
 
     @Test
-    fun part1() {
-        assertEquals(1, foo(File("files/2021/day1.txt").readText()))
+    fun part2() {
+        assertEquals(5, numTimesSumTripletsIncreases("199\n200\n208\n210\n200\n207\n240\n269\n260\n263"))
+        assertEquals(1822, numTimesSumTripletsIncreases(File("files/2021/day1.txt").readText()))
     }
-
-//    @Test
-//    fun part2() {
-//        assertEquals(1, bar(File("files/2021/day1.txt").readText()))
-//    }
 }
