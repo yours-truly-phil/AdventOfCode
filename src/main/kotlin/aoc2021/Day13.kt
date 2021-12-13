bef10a7fb44a58734134cfcb095a1c1dd133d37a
Philipp Seeger <pseeger@k15t.com>
2021-12-13 08:56:31 +0100

    Use data class for equals and hash code

--- src/main/kotlin/aoc2021/Day13.kt
+++ src/main/kotlin/aoc2021/Day13.kt
@@ -66,25 +66,7 @@ class Day13 {
         }
     }
 
-    class Vec2i(val x: Int, val y: Int) {
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (javaClass != other?.javaClass) return false
-
-            other as Vec2i
-
-            if (x != other.x) return false
-            if (y != other.y) return false
-
-            return true
-        }
-
-        override fun hashCode(): Int {
-            var result = x
-            result = 31 * result + y
-            return result
-        }
-    }
+    data class Vec2i(val x: Int, val y: Int)
 
     @Test
     fun part1() {
