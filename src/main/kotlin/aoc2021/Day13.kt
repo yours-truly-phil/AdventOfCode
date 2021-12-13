4325db29dcb3bcc7522bf01301234d9d2f3d9092
Philipp Seeger <pseeger@k15t.com>
2021-12-13 08:55:16 +0100

    AoC 2021 Day 13

---
+++ src/main/kotlin/aoc2021/Day13.kt
@@ -0,0 +1,130 @@
+package aoc2021
+
+import org.junit.jupiter.api.Test
+import java.io.File
+import kotlin.test.assertEquals
+
+class Day13 {
+    private fun solve(input: String, isPart1: Boolean): Int {
+        val (points, instructions) = input.split("\n\n")
+        val vPoints = points.lines().map {
+            val (x, y) = it.split(",").map { i -> i.toInt() }
+            Vec2i(x, y)
+        }.toHashSet()
+
+        val map = Map(vPoints)
+//        println("$map\n")
+        instructions.lines().forEach {
+            val (axis, numString) = it.split(" ").last().split("=")
+            val num = numString.toInt()
+            if (axis == "y") {
+                map.foldY(num)
+            } else {
+                map.foldX(num)
+            }
+            println("Number of points: ${map.map.size}")
+            if (isPart1) {
+                return map.map.size
+            }
+        }
+        println("$map\n")
+        return -1
+    }
+
+    class Map(val map: MutableSet<Vec2i>) {
+        fun foldY(axis: Int) {
+            val pointsToFold = map.filter { it.y > axis }
+            pointsToFold.forEach {
+                map.remove(it)
+                map.add(Vec2i(it.x, axis - (it.y - axis)))
+            }
+        }
+
+        fun foldX(axis: Int) {
+            val pointsToFold = map.filter { it.x > axis }
+            pointsToFold.forEach {
+                map.remove(it)
+                map.add(Vec2i(axis - (it.x - axis), it.y))
+            }
+        }
+
+        override fun toString(): String {
+            val maxX = map.maxByOrNull { it.x }!!.x
+            val maxY = map.maxByOrNull { it.y }!!.y
+            val sb = StringBuilder()
+            for (y in 0..maxY) {
+                for (x in 0..maxX) {
+                    if (map.contains(Vec2i(x, y))) {
+                        sb.append("#")
+                    } else {
+                        sb.append(".")
+                    }
+                }
+                sb.append("\n")
+            }
+            return sb.toString()
+        }
+    }
+
+    class Vec2i(val x: Int, val y: Int) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Vec2i
+
+            if (x != other.x) return false
+            if (y != other.y) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = x
+            result = 31 * result + y
+            return result
+        }
+    }
+
+    @Test
+    fun part1() {
+        assertEquals(
+            17, solve(
+                """
+6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5""".trimIndent(), true
+            )
+        )
+        assertEquals(669, solve(File("files/2021/day13.txt").readText(), true))
+    }
+
+    @Test
+    fun part2() {
+        //#..#.####.####.####..##..#..#..##....##
+        //#..#.#....#.......#.#..#.#..#.#..#....#
+        //#..#.###..###....#..#....#..#.#.......#
+        //#..#.#....#.....#...#....#..#.#.......#
+        //#..#.#....#....#....#..#.#..#.#..#.#..#
+        //.##..####.#....####..##...##...##...##.
+        assertEquals(-1, solve(File("files/2021/day13.txt").readText(), false))
+    }
+}
