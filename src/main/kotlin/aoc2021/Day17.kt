8794bd464c7153e0f20b1a0f1af74de18d34976a
Philipp Seeger <pseeger@k15t.com>
2021-12-17 09:38:20 +0100

    Remove duplicate code

--- src/main/kotlin/aoc2021/Day17.kt
+++ src/main/kotlin/aoc2021/Day17.kt
@@ -7,14 +7,7 @@ import kotlin.math.sqrt
 import kotlin.test.assertEquals
 
 class Day17 {
-    private fun solvePart1(input: String): Int {
-        val parts = input.substring(13).split(", ")
-        val (x1, x2) = parts[0].substring(2).split("..").map { it.toInt() }
-        val (y1, y2) = parts[1].substring(2).split("..").map { it.toInt() }
-        println("x1=$x1, x2=$x2, y1=$y1, y2=$y2")
-        val maxYVelocity = y1.absoluteValue - 1
-        return (maxYVelocity * (maxYVelocity + 1)) / 2
-    }
+    private fun solvePart1(input: String): Int = parseInput(input).let { it.maxYVelocity * (it.maxYVelocity + 1) / 2 }
 
     class Probe(var x: Int, var y: Int, var vX: Int, var vY: Int) {
         fun next() {
@@ -26,41 +19,43 @@ class Day17 {
             }
         }
 
-        fun isCollision(x1: Int, x2: Int, y1: Int, y2: Int): Boolean {
-            return x in x1..x2 && y in y1..y2
-        }
+        fun isCollision(x1: Int, x2: Int, y1: Int, y2: Int): Boolean = x in x1..x2 && y in y1..y2
     }
 
-    private fun solvePart2(input: String): Int {
+    class E(
+        val x1: Int, val x2: Int, val y1: Int, val y2: Int, val maxYVelocity: Int, val maxXVelocity: Int,
+        val minYVelocity: Int, val minXVelocity: Int
+    )
+
+    private fun parseInput(input: String): E {
         val parts = input.substring(13).split(", ")
         val (x1, x2) = parts[0].substring(2).split("..").map { it.toInt() }
         val (y1, y2) = parts[1].substring(2).split("..").map { it.toInt() }
-        println("x1=$x1, x2=$x2, y1=$y1, y2=$y2")
         val maxYVelocity = y1.absoluteValue - 1
         val maxXVelocity = x2.absoluteValue
-        val minXVelocity = quadr(1.0, 1.0, -2.0 * y2.absoluteValue)[0].roundToInt()
-        println("maxYVelocity=$maxYVelocity,  maxXVelocity=$maxXVelocity, minXVelocity=$minXVelocity")
-        var count = 0
-        for (y in y1..maxYVelocity) {
-            for (x in minXVelocity..maxXVelocity) {
-                val probe = Probe(0, 0, x, y)
-                while (probe.x <= x2 && probe.y >= y1) {
-                    probe.next()
-                    if (probe.isCollision(x1, x2, y1, y2)) {
-                        count++
-                        break
-                    }
-                }
-            }
-        }
-        return count
+        val minXVelocity = quadr(-2.0 * y2.absoluteValue).roundToInt()
+        return E(x1, x2, y1, y2, maxYVelocity, maxXVelocity, y1, minXVelocity)
     }
 
-    private fun quadr(a: Double, b: Double, c: Double): Array<Double> {
-        return arrayOf(
-            (-b + sqrt(b * b - 4 * a * c)) / (2 * a),
-            (-b - sqrt(b * b - 4 * a * c)) / (2 * a)
-        )
+    private fun quadr(c: Double): Double = (-1.0 + sqrt(1 - 4 * c)) * 0.5
+
+    private fun solvePart2(input: String): Int {
+        val e = parseInput(input)
+        var count = 0
+        (e.minYVelocity..e.maxYVelocity).forEach { y ->
+            (e.minXVelocity..e.maxXVelocity)
+                .map { x -> Probe(0, 0, x, y) }
+                .forEach {
+                    while (it.x <= e.x2 && it.y >= e.y1) {
+                        it.next()
+                        if (it.isCollision(e.x1, e.x2, e.y1, e.y2)) {
+                            count++
+                            break
+                        }
+                    }
+                }
+        }
+        return count
     }
 
     @Test
