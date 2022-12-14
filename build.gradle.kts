104ba63f6dec2c77807563ae98d9e6ea50b3eb54
Philipp Seeger <pseeger@k15t.com>
2022-12-14 16:57:01 +0100

    Bump up versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.20-M1"
+    kotlin("jvm") version "1.7.20"
     application
 }
 
@@ -13,15 +13,15 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    implementation("com.google.code.gson:gson:2.8.9")
-    implementation("com.google.guava:guava:31.0.1-jre")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("com.google.code.gson:gson:2.10")
+    implementation("com.google.guava:guava:31.1-jre")
     implementation("org.jetbrains.kotlin:kotlin-test-junit5:1.6.20-M1")
-    implementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    implementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.6.20-M1")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
     implementation("org.jetbrains.kotlinx:multik-api:0.1.1")
     implementation("org.jetbrains.kotlinx:multik-jvm:0.1.1")
 }
